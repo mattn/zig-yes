ac458550a0d4a1349e83cb7d9befb6dc67d4e47b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-07-25 23:41:23 +0900

    first import

---
+++ src/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn main() anyerror!void {
+    const a = std.heap.page_allocator;
+
+    const args = try std.process.argsAlloc(a);
+    defer a.free(args);
+
+    var bytes = std.ArrayList(u8).init(a);
+    defer bytes.deinit();
+    if (args.len > 1) {
+        for (args[1..args.len]) |arg, i| {
+            if (i > 0) try bytes.writer().writeAll(" ");
+            try bytes.writer().writeAll(arg);
+        }
+    } else {
+        try bytes.writer().writeAll("y");
+    }
+    try bytes.writer().print("\n", .{});
+
+    var buf = std.ArrayList(u8).init(a);
+    const bufsiz = 64 * 1024;
+    var copysize = bytes.items.len;
+
+    var copies = bufsiz / copysize;
+    while (copies > 0) {
+        try buf.writer().writeAll(bytes.items);
+        copies -= 1;
+    }
+
+    const writer = std.io.getStdOut().writer();
+    while (true) {
+        try writer.writeAll(buf.items);
+    }
+}
